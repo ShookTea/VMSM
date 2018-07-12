29f02ab98b7f91138343e1cfd12f592705a64b59
ShookTea <kowaliknor@gmail.com>
2018-07-12 19:38:31 +0200

    New line

--- src/eu/shooktea/vmsm/Start.java
+++ src/eu/shooktea/vmsm/Start.java
@@ -27,6 +27,7 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Start extends Application {
+    
     @Override
     public void start(Stage primaryStage) throws Exception {
         
