2d9619af480cb2c8d297a95c4b832e7bbad7abe5
Norbert Kowalik <nkowalik@divante.pl>
2018-07-18 08:53:36 +0200

    Displaying modules

--- src/eu/shooktea/vmsm/view/controller/ModuleConfig.java
+++ src/eu/shooktea/vmsm/view/controller/ModuleConfig.java
@@ -6,42 +6,59 @@ import eu.shooktea.vmsm.module.Module;
 import eu.shooktea.vmsm.vmtype.VMType;
 import javafx.beans.binding.When;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ToggleButton;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 import javafx.scene.text.Font;
 
 
 public class ModuleConfig {
-    @FXML private VBox modulesList;
+    @FXML private GridPane grid;
 
     @FXML
     private void initialize() {
+        grid.setStyle("-fx-background-color: lightgray; -fx-vgap: 1; -fx-hgap: 1; -fx-padding: 1;");
+
         VirtualMachine vm = Start.virtualMachineProperty.getValue();
         VMType type = vm.getType();
         Module[] modules = type.getModules().get();
+        int index = 0;
         for (Module module : modules) {
-            createModuleEntry(module);
+            createModuleEntry(module, index);
+            index += 2;
         }
+        ColumnConstraints textColumn = new ColumnConstraints();
+        textColumn.setHgrow(Priority.ALWAYS);
+        grid.getColumnConstraints().add(textColumn);
     }
 
-    private void createModuleEntry(Module module) {
+    private void createModuleEntry(Module module, int rowIndex) {
         Label name = new Label(module.getName());
         name.setFont(new Font(22));
+        name.setMaxWidth(Double.MAX_VALUE);
+
         Label description = new Label(module.getDescription());
+        description.setMaxWidth(Double.MAX_VALUE);
+
         ToggleButton switchButton = new ToggleButton();
         switchButton.textProperty().bind(
                 new When(switchButton.selectedProperty())
                 .then("On")
                 .otherwise("Off"));
-
         switchButton.styleProperty().bind(
                 new When(switchButton.selectedProperty())
                 .then("-fx-background-color: green;")
                 .otherwise("-fx-background-color: red;"));
+        switchButton.setMaxWidth(Double.MAX_VALUE);
 
-        VBox elements = new VBox(name, description, switchButton);
-        modulesList.getChildren().add(elements);
+        Button configButton = new Button("Config");
+        configButton.setMaxWidth(Double.MAX_VALUE);
+
+        grid.addRow(rowIndex, name, switchButton);
+        grid.addRow(rowIndex + 1, description, configButton);
     }
 
     public static void openModuleConfigWindow(Object... lambdaArgs) {
