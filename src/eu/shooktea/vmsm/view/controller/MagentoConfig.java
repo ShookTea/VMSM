cd68021c533c2560f69d1f86a619437d2ce342e9
Norbert Kowalik <nkowalik@divante.pl>
2018-07-19 08:57:12 +0200

    Loading and saving admin login data

--- src/eu/shooktea/vmsm/view/controller/MagentoConfig.java
+++ src/eu/shooktea/vmsm/view/controller/MagentoConfig.java
@@ -6,6 +6,7 @@ import eu.shooktea.vmsm.VirtualMachine;
 import eu.shooktea.vmsm.module.Module;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 import javafx.stage.DirectoryChooser;
@@ -15,15 +16,23 @@ import java.io.File;
 
 public class MagentoConfig implements StageController {
     @FXML private TextField magentoPath;
+    @FXML private TextField adminLogin;
+    @FXML private PasswordField adminPassword;
     @FXML private Label magentoInfo;
 
     @FXML
     private void initialize() {
         VirtualMachine vm = Start.virtualMachineProperty.getValue();
         Module module = Module.getModuleByName("Magento");
-        String path = module.getSetting(vm, "path");
-        if (path != null) {
-            magentoPath.setText(path);
+        loadSetting(module, vm, magentoPath, "path");
+        loadSetting(module, vm, adminLogin, "adm_login");
+        loadSetting(module, vm, adminPassword, "adm_pass");
+    }
+
+    private void loadSetting(Module module, VirtualMachine vm, TextField field, String name) {
+        String value = module.getSetting(vm, name);
+        if (value != null) {
+            field.setText(value);
         }
     }
 
@@ -52,18 +61,29 @@ public class MagentoConfig implements StageController {
     private void saveSettings() {
         VirtualMachine vm = Start.virtualMachineProperty.getValue();
         Module module = Module.getModuleByName("Magento");
-        String trim = magentoPath.getText().trim();
-        if (trim.isEmpty()) {
-            module.removeSetting(vm, "path");
-        }
-        File file = new File(trim);
+        File file = new File(magentoPath.getText().trim());
         if (!checkFile(file)) {
             module.removeSetting(vm, "path");
         }
-        module.setSetting(vm, "path", file.toString());
+        else {
+            saveConf(magentoPath, "path", module, vm);
+        }
+
+        saveConf(adminLogin, "adm_login", module, vm);
+        saveConf(adminPassword, "adm_pass", module, vm);
 
         Storage.saveAll();
         stage.close();
+    }
+
+    private void saveConf(TextField textField, String name, Module module, VirtualMachine vm) {
+        String trim = textField.getText().trim();
+        if (trim.isEmpty()) {
+            module.removeSetting(vm, name);
+        }
+        else {
+            module.setSetting(vm, name, trim);
+        }
     }
 
     private boolean checkFile(File file) {
