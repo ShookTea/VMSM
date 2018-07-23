2e747d18785e8844164caf079c971ef3fddbe3e9
Norbert Kowalik <nkowalik@divante.pl>
2018-07-23 13:22:51 +0200

    Registering SSH module

---
+++ src/eu/shooktea/vmsm/module/SSH.java
@@ -0,0 +1,13 @@
+package eu.shooktea.vmsm.module;
+
+public class SSH extends Module {
+    @Override
+    public String getName() {
+        return "SSH";
+    }
+
+    @Override
+    public String getDescription() {
+        return "SSH client";
+    }
+}
