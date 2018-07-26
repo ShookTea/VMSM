ae41332263aabdda1c850f13ffe34cebe26940d8
Norbert Kowalik <nkowalik@divante.pl>
2018-07-26 09:35:18 +0200

    Toolbar DB button

--- src/eu/shooktea/vmsm/module/MySQL.java
+++ src/eu/shooktea/vmsm/module/MySQL.java
@@ -1,7 +1,19 @@
 package eu.shooktea.vmsm.module;
 
+import eu.shooktea.vmsm.Toolkit;
+import eu.shooktea.vmsm.VM;
+import eu.shooktea.vmsm.view.View;
 import eu.shooktea.vmsm.view.controller.mysql.MysqlConfig;
+import eu.shooktea.vmsm.view.controller.ssh.SshTerminal;
+import javafx.application.Platform;
+import javafx.geometry.Orientation;
+import javafx.scene.Node;
+import javafx.scene.control.Separator;
+import javafx.scene.control.Tooltip;
+import javafx.scene.image.ImageView;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class MySQL extends Module {
@@ -28,7 +40,44 @@ public class MySQL extends Module {
     }
 
     @Override
+    public void afterModuleRemoved() {
+        super.afterModuleRemoved();
+        if (toolbarElements == null) toolbarElements = createToolbarElements();
+        View.controller().toolBar.getItems().removeAll(toolbarElements);
+    }
+
+    @Override
+    public void afterModuleTurnedOff() {
+        super.afterModuleTurnedOff();
+        if (toolbarElements == null) toolbarElements = createToolbarElements();
+        Platform.runLater(() -> {
+            View.controller().toolBar.getItems().removeAll(toolbarElements);
+        });
+    }
+
+    @Override
+    public void reloadToolbar() {
+        if (toolbarElements == null) toolbarElements = createToolbarElements();
+        View.controller().toolBar.getItems().addAll(toolbarElements);
+    }
+
+    private List<Node> createToolbarElements() {
+        ImageView openTerminal = Toolkit.createToolbarImage("db-blue.png");
+        Tooltip removeCacheTip = new Tooltip("Open MySQL");
+        Tooltip.install(openTerminal, removeCacheTip);
+        openTerminal.setOnMouseClicked(SshTerminal::openSshTerminal);
+
+        List<Node> nodes = new ArrayList<>();
+        if (!SSH.getModuleByName("SSH").isInstalled(VM.getOrThrow()))
+            nodes.add(new Separator(Orientation.VERTICAL));
+        nodes.add(openTerminal);
+        return nodes;
+    }
+
+    @Override
     public int toolbarOrder() {
         return 1001;
     }
+
+    private List<Node> toolbarElements;
 }
