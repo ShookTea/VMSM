a6c728d3a10c605230641b491567b39a8161e952
Norbert Kowalik <nkowalik@divante.pl>
2018-07-31 07:37:57 +0200

    Opening admin creation window

--- src/eu/shooktea/vmsm/view/controller/mage/CreateNewAdmin.java
+++ src/eu/shooktea/vmsm/view/controller/mage/CreateNewAdmin.java
@@ -3,10 +3,50 @@ package eu.shooktea.vmsm.view.controller.mage;
 import eu.shooktea.vmsm.VM;
 import eu.shooktea.vmsm.VirtualMachine;
 import eu.shooktea.vmsm.module.MySQL;
+import eu.shooktea.vmsm.module.SqlConnection;
+import eu.shooktea.vmsm.view.View;
+import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 
 public class CreateNewAdmin {
+    @FXML private TextField login;
+    @FXML private PasswordField password;
+    @FXML private TextField salt;
+    @FXML private TextField name;
+    @FXML private TextField surname;
+    @FXML private TextField email;
+    @FXML private Label error;
+
+
+    @FXML
+    private void create() {
+        if (login.getText().trim().isEmpty() || password.getText().isEmpty()) {
+            error.setText("Login and password are required values.");
+            return;
+        }
+        error.setText("");
+
+        String login = this.login.getText();
+        String password = this.password.getText();
+        String salt = this.salt.getText();
+        String name = this.name.getText();
+        String surname = this.surname.getText();
+
+        MySQL sql = MySQL.getModuleByName("MySQL");
+        VirtualMachine vm = VM.getOrThrow();
+        SqlConnection connection = sql.createConnection();
+        try {
+            connection.open();
+
+            connection.close();
+        } catch (Exception ex) {
+            error.setText("Exception: " + ex.getMessage());
+        }
+    }
 
     public static void openAdminCreationWindow(Object... lambdaArgs) {
         MySQL sql = MySQL.getModuleByName("MySQL");
@@ -18,7 +58,7 @@ public class CreateNewAdmin {
             dialog.showAndWait();
         }
         else {
-
+            View.createNewWindow("/eu/shooktea/vmsm/view/fxml/mage/CreateNewAdmin.fxml", "Create new admin", true);
         }
     }
 }
