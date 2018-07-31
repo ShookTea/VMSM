eda6e538ee8583437e895f38a9ff61aa61d9688e
Norbert Kowalik <nkowalik@divante.pl>
2018-07-31 08:07:43 +0200

    Creating new admin

--- src/eu/shooktea/vmsm/view/controller/mage/CreateNewAdmin.java
+++ src/eu/shooktea/vmsm/view/controller/mage/CreateNewAdmin.java
@@ -1,18 +1,23 @@
 package eu.shooktea.vmsm.view.controller.mage;
 
+import eu.shooktea.vmsm.Storage;
 import eu.shooktea.vmsm.VM;
 import eu.shooktea.vmsm.VirtualMachine;
+import eu.shooktea.vmsm.module.Magento;
 import eu.shooktea.vmsm.module.MySQL;
 import eu.shooktea.vmsm.module.SqlConnection;
 import eu.shooktea.vmsm.view.View;
+import eu.shooktea.vmsm.view.controller.StageController;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
 
 
-public class CreateNewAdmin {
+public class CreateNewAdmin implements StageController {
     @FXML private TextField login;
     @FXML private PasswordField password;
     @FXML private TextField salt;
@@ -20,7 +25,7 @@ public class CreateNewAdmin {
     @FXML private TextField surname;
     @FXML private TextField email;
     @FXML private Label error;
-
+    private Stage stage;
 
     @FXML
     private void create() {
@@ -30,21 +35,66 @@ public class CreateNewAdmin {
         }
         error.setText("");
 
-        String login = this.login.getText();
+        String login = this.login.getText().trim();
         String password = this.password.getText();
-        String salt = this.salt.getText();
-        String name = this.name.getText();
-        String surname = this.surname.getText();
+        String salt = this.salt.getText().trim();
+        String name = this.name.getText().trim();
+        String surname = this.surname.getText().trim();
+        String email = this.email.getText().trim();
+
+        if (salt.isEmpty()) salt = "salt";
+        if (name.isEmpty()) name = "John";
+        if (surname.isEmpty()) surname = "Smith";
+        if (email.isEmpty()) email = "john@smith.com";
 
         MySQL sql = MySQL.getModuleByName("MySQL");
         VirtualMachine vm = VM.getOrThrow();
-        SqlConnection connection = sql.createConnection();
+        connection = sql.createConnection();
         try {
             connection.open();
-
+            query("LOCK TABLES `admin_role` WRITE, `admin_user` WRITE");
+            query("SET @SALT = \"" + salt + "\"");
+            query("SET @PASS = CONCAT(MD5(CONCAT(@SALT, \"" + password + "\")), CONCAT(\":\", @SALT))");
+            query("SELECT @EXTRA := MAX(extra) FROM admin_user WHERE extra IS NOT NULL");
+            query("INSERT INTO `admin_user` (firstname, lastname, email, username, password, created, lognum, reload_acl_flag, is_active, extra, rp_token_created_at) " +
+                    "VALUES ('" + name + "','" + surname + "','" + email + "','" + login + "',@PASS,NOW(),0,0,1,@EXTRA,NOW())");
+            query("INSERT INTO `admin_role` (parent_id, tree_level, sort_order, role_type, user_id, role_name) " +
+                    "VALUES(1,2,0,'U',(SELECT user_id FROM admin_user WHERE username = '" + login + "'),'" + name + "')");
+            query("UNLOCK TABLES");
             connection.close();
+
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Admin created");
+            alert.setHeaderText("Admin account created");
+            alert.setContentText("Do you want to set it as default account?");
+            ButtonType yes = new ButtonType("Yes", ButtonBar.ButtonData.YES);
+            ButtonType no = new ButtonType("No", ButtonBar.ButtonData.NO);
+            alert.getButtonTypes().setAll(yes, no);
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.get() == yes) {
+                Magento magento = Magento.getModuleByName("Magento");
+                magento.setSetting(vm, "adm_login", login);
+                magento.setSetting(vm, "adm_pass", password);
+                Storage.saveAll();
+            }
+            stage.close();
         } catch (Exception ex) {
             error.setText("Exception: " + ex.getMessage());
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
+    }
+    SqlConnection connection;
+
+    private void query(String query) throws SQLException {
+        Object ob = connection.query(query);
+        if (ob instanceof ResultSet) {
+            ResultSet result = (ResultSet)ob;
+            result.close();
         }
     }
 
@@ -60,5 +110,10 @@ public class CreateNewAdmin {
         else {
             View.createNewWindow("/eu/shooktea/vmsm/view/fxml/mage/CreateNewAdmin.fxml", "Create new admin", true);
         }
+    }
+
+    @Override
+    public void setStage(Stage stage) {
+        this.stage = stage;
     }
 }
