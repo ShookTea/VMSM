279eadb20fc38763f0bdf11ac37f92542f1a3f4f
Norbert Kowalik <nkowalik@divante.pl>
2018-08-01 08:13:29 +0200

    Testing menu

--- src/eu/shooktea/vmsm/view/controller/SimpleGuiController.java
+++ src/eu/shooktea/vmsm/view/controller/SimpleGuiController.java
@@ -1,11 +1,16 @@
 package eu.shooktea.vmsm.view.controller;
 
+import eu.shooktea.vmsm.view.View;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.input.MouseEvent;
 
 public class SimpleGuiController {
     private SimpleGuiController() {}
 
     public static void openGui(MouseEvent e) {
-        System.out.println("Clicked!");
+        MenuItem item = new MenuItem("Test");
+        ContextMenu menu = new ContextMenu(item);
+        menu.show(View.stage(), e.getX(), e.getY());
     }
 }
