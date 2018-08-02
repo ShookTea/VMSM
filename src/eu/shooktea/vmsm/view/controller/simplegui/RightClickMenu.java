fd419b0b0d2358e3488b54ae7a5537485d35a13e
Norbert Kowalik <nkowalik@divante.pl>
2018-08-02 12:16:46 +0200

    VM select menu

--- src/eu/shooktea/vmsm/view/controller/simplegui/RightClickMenu.java
+++ src/eu/shooktea/vmsm/view/controller/simplegui/RightClickMenu.java
@@ -1,11 +1,15 @@
 package eu.shooktea.vmsm.view.controller.simplegui;
 
+import eu.shooktea.vmsm.Storage;
 import eu.shooktea.vmsm.Toolkit;
-import javafx.scene.control.ContextMenu;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.SeparatorMenuItem;
+import eu.shooktea.vmsm.VM;
+import eu.shooktea.vmsm.VirtualMachine;
+import eu.shooktea.vmsm.view.controller.NewVM;
+import eu.shooktea.vmsm.view.controller.VmManager;
+import javafx.scene.control.*;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class RightClickMenu {
@@ -19,6 +23,7 @@ public class RightClickMenu {
 
     private ContextMenu createContextMenu() {
         List<MenuItem> list = new ArrayList<>();
+        list.add(vmMenu());
         list.add(exit());
 
         ContextMenu menu = new ContextMenu();
@@ -30,10 +35,55 @@ public class RightClickMenu {
         return new SeparatorMenuItem();
     }
 
+    private MenuItem newVm() {
+        MenuItem item = new MenuItem("Create new VM...");
+        item.setOnAction(NewVM::openNewVmWindow);
+        return item;
+    }
+
     private MenuItem exit() {
         MenuItem item = new MenuItem("Exit VMSM");
         item.setGraphic(Toolkit.createMenuImage("exit.png"));
         item.setOnAction(e -> System.exit(0));
+        return item;
+    }
+
+    private MenuItem vmMenu() {
+        if (!VM.isSet()) return newVm();
+
+        List<MenuItem> items = new ArrayList<>();
+        ToggleGroup group = vmToggleGroup();
+        for (VirtualMachine vm : Storage.getVmList().sorted(Comparator.comparing(VirtualMachine::getName))) {
+            RadioMenuItem vmItem = new RadioMenuItem(vm.getName());
+            vmItem.setToggleGroup(group);
+            if (VM.isEqual(vm)) group.selectToggle(vmItem);
+            items.add(vmItem);
+        }
+        items.add(separator());
+        items.add(newVm());
+        items.add(vmManager());
+        Menu menu = new Menu("VMs");
+        menu.getItems().addAll(items);
+        return menu;
+    }
+
+    private ToggleGroup vmToggleGroup() {
+        ToggleGroup group = new ToggleGroup();
+        group.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
+            RadioMenuItem item = (RadioMenuItem)newValue;
+            String name = item.getText();
+            VirtualMachine chosenMachine = Storage.getVmList().stream()
+                    .filter(vm -> vm.getName().equals(name))
+                    .findFirst()
+                    .get();
+            VM.set(chosenMachine);
+        });
+        return group;
+    }
+
+    private MenuItem vmManager() {
+        MenuItem item = new MenuItem("VM Manager...");
+        item.setOnAction(VmManager::openVmManagerWindow);
         return item;
     }
 
