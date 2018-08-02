6fc2a7282eb4424fee019d37fd8ab9cf7c042bb6
Norbert Kowalik <nkowalik@divante.pl>
2018-08-02 13:19:54 +0200

    Going to home page

--- src/eu/shooktea/vmsm/view/controller/simplegui/QuickGuiMenu.java
+++ src/eu/shooktea/vmsm/view/controller/simplegui/QuickGuiMenu.java
@@ -7,6 +7,10 @@ import javafx.event.EventHandler;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,7 +36,7 @@ public class QuickGuiMenu {
 
         VM.ifNotNullOrElse(
                 vm -> {
-
+                    if (vm.getPageRoot() != null) list.add(home(vm.getPageRoot()));
                 },
                 () -> list.add(createNewVmButton())
         );
@@ -51,6 +55,23 @@ public class QuickGuiMenu {
         ImageView exitButton = Toolkit.createQuickGuiButton("terminal.png", "Create new VM");
         exitButton.setOnMouseClicked(NewVM::openNewVmWindow);
         return exitButton;
+    }
+
+    private ImageView home(URL home) {
+        return toWebPage(home, "home.png", "Go to home page");
+    }
+
+    private ImageView toWebPage(URL url, String fileName, String tooltip) {
+        ImageView iv = Toolkit.createQuickGuiButton(fileName, tooltip);
+        iv.setOnMouseClicked(e -> {
+            try {
+                Desktop.getDesktop().browse(url.toURI());
+            } catch (IOException | URISyntaxException e1) {
+                e1.printStackTrace();
+                System.exit(1);
+            }
+        });
+        return iv;
     }
 
     private final List<ImageView> list;
