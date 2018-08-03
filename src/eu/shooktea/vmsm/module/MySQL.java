bf3337161909f787d55082c14bdd2424349e30f6
Norbert Kowalik <nkowalik@divante.pl>
2018-08-03 12:11:25 +0200

    MySQL module is now using SimpleGui

--- src/eu/shooktea/vmsm/module/MySQL.java
+++ src/eu/shooktea/vmsm/module/MySQL.java
@@ -1,8 +1,15 @@
 package eu.shooktea.vmsm.module;
 
+import eu.shooktea.vmsm.Toolkit;
 import eu.shooktea.vmsm.VM;
 import eu.shooktea.vmsm.view.controller.mysql.MysqlConfig;
+import eu.shooktea.vmsm.view.controller.mysql.MysqlTerminal;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.ImageView;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 public class MySQL extends Module {
@@ -28,5 +35,26 @@ public class MySQL extends Module {
     @Override
     public int getSortValue() {
         return 1005;
+    }
+
+    @Override
+    public Optional<List<ImageView>> getQuickGuiButtons() {
+        ImageView openTerminal = Toolkit.createQuickGuiButton("db-blue.png", "Open MySQL terminal");
+        openTerminal.setOnMouseClicked(MysqlTerminal::openMysqlTerminal);
+        return Optional.of(Arrays.asList(openTerminal));
+    }
+
+    @Override
+    public Optional<MenuItem> getMenuItem() {
+        Menu root = new Menu("MySQL", Toolkit.createMenuImage("db-blue.png"));
+
+        MenuItem openTerminal = new MenuItem("Open terminal...");
+        openTerminal.setOnAction(MysqlTerminal::openMysqlTerminal);
+
+        MenuItem config = new MenuItem("MySQL configuration...", Toolkit.createMenuImage("run.png"));
+        config.setOnAction(MysqlConfig::openMysqlConfigWindow);
+
+        root.getItems().addAll(openTerminal, config);
+        return Optional.of(root);
     }
 }
