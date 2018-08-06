d3377ff89df7693b9c5f7eae70dd3540f3bea908
Norbert Kowalik <nkowalik@divante.pl>
2018-08-06 13:10:46 +0200

    Screen selection

--- src/eu/shooktea/vmsm/view/controller/Config.java
+++ src/eu/shooktea/vmsm/view/controller/Config.java
@@ -1,10 +1,43 @@
 package eu.shooktea.vmsm.view.controller;
 
 import eu.shooktea.vmsm.view.View;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.util.StringConverter;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Config implements StageController {
+    @FXML private ChoiceBox<Screen> displayScreen;
+
+    private Map<String, Screen> screenMap;
     private Stage stage;
+
+    @FXML
+    private void initialize() {
+        ObservableList<Screen> screens = Screen.getScreens();
+        displayScreen.setItems(screens);
+        screenMap = new HashMap<>();
+        StringConverter<Screen> converter = new StringConverter<Screen>() {
+            @Override
+            public String toString(Screen object) {
+                return "Screen " + (screens.indexOf(object) + 1);
+            }
+
+            @Override
+            public Screen fromString(String string) {
+                return screenMap.get(string);
+            }
+        };
+        for (Screen screen : screens) {
+            screenMap.put(converter.toString(screen), screen);
+        }
+        displayScreen.setConverter(converter);
+    }
 
 
     @Override
