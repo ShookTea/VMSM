806baa289ba73c3df25524e3ba143aafd805400f
Norbert Kowalik <nkowalik@divante.pl>
2018-08-06 15:04:54 +0200

    eu.shooktea.vmsm package-info

---
+++ src/eu/shooktea/vmsm/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Main package of VMSM. It contains basic classes used by all VM types and modules, except for view, which are
+ * stored {@link eu.shooktea.vmsm.view} package.
+ */
+package eu.shooktea.vmsm;
