e508a351e4d348b63669d0676663431b2c5acf4c
Norbert Kowalik <nkowalik@divante.pl>
2018-08-07 13:32:07 +0200

    Start working on better SSH terminal

--- src/eu/shooktea/vmsm/module/ssh/SshConnection.java
+++ src/eu/shooktea/vmsm/module/ssh/SshConnection.java
@@ -1,6 +1,12 @@
 package eu.shooktea.vmsm.module.ssh;
 
 import com.jcraft.jsch.ChannelShell;
+import com.jcraft.jsch.JSchException;
+import javafx.application.Platform;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyEvent;
+
+import java.io.*;
 
 /**
  * Class representing single SSH connection.
@@ -12,11 +18,36 @@ public class SshConnection {
 
     /**
      * Initializes SSH connection.
+     * @throws IOException when piping input stream has failed
+     * @throws JSchException when connection to SSH has failed
      */
-    public void init() {
+    public void init() throws IOException, JSchException {
         if (shell == null) return;
+        shell.setAgentForwarding(true);
+        Console console = new Console();
+        shell.setOutputStream(new PrintStream(console, true));
 
+        PipedInputStream pin = new PipedInputStream();
+        pout = new PipedOutputStream(pin);
+        shell.setInputStream(pin);
+
+        shell.connect(300);
+    }
+
+    public void keyTyped(KeyEvent event) {
+        System.out.println("KeyCode = " + event.getCode() + ", Character = " + event.getCharacter());
     }
 
     private final ChannelShell shell;
+    private PipedOutputStream pout;
+    private String consoleDisplay = "";
+    private String input = "";
+
+    private class Console extends OutputStream {
+        public void write(int i) {
+            Platform.runLater(() -> {
+                consoleDisplay += String.valueOf((char)i);
+            });
+        }
+    }
 }
