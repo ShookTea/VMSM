56d1f9dc9cae3dbac42b88c76a6e7e93ee4cf710
Norbert Kowalik <nkowalik@divante.pl>
2018-08-08 09:08:48 +0200

    Displaying tables

--- src/eu/shooktea/vmsm/view/controller/mysql/TabScreen.java
+++ src/eu/shooktea/vmsm/view/controller/mysql/TabScreen.java
@@ -2,32 +2,18 @@ package eu.shooktea.vmsm.view.controller.mysql;
 
 import eu.shooktea.vmsm.module.mysql.TableContent;
 import eu.shooktea.vmsm.module.mysql.TableEntry;
+import eu.shooktea.vmsm.view.View;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.VBox;
 
-import java.io.IOException;
 
 public class TabScreen extends VBox {
 
     private @FXML TableView<TableEntry> dataTable;
-
-    public TabScreen() {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
-                "/eu/shooktea/vmsm/view/fxml/mysql/TabScreen.fxml"
-        ));
-        fxmlLoader.setRoot(this);
-        fxmlLoader.setController(this);
-        try {
-            fxmlLoader.load();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
     public void setDataTableContent(TableContent content) {
         this.content = content;
@@ -44,6 +30,10 @@ public class TabScreen extends VBox {
 
     public TableContent getDataTableContent() {
         return content;
+    }
+
+    public static TabScreen showTabScreen(Object... lambdaArgs) {
+        return View.createNewWindow("/eu/shooktea/vmsm/view/fxml/mysql/TabScreen.fxml", "Database");
     }
 
     private TableContent content;
