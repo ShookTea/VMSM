a6c2844ac7f84dc0a08259c5eec7dc28d406d3c7
Norbert Kowalik <nkowalik@divante.pl>
2018-08-08 09:48:24 +0200

    Handling connection

--- src/eu/shooktea/vmsm/view/controller/mysql/TabScreen.java
+++ src/eu/shooktea/vmsm/view/controller/mysql/TabScreen.java
@@ -1,19 +1,42 @@
 package eu.shooktea.vmsm.view.controller.mysql;
 
+import com.jcraft.jsch.JSchException;
+import eu.shooktea.vmsm.module.mysql.MySQL;
+import eu.shooktea.vmsm.module.mysql.SqlConnection;
 import eu.shooktea.vmsm.module.mysql.TableContent;
 import eu.shooktea.vmsm.module.mysql.TableEntry;
+import eu.shooktea.vmsm.view.StageController;
 import eu.shooktea.vmsm.view.View;
+import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 
-public class TabScreen extends VBox {
+public class TabScreen implements StageController {
 
     private @FXML TableView<TableEntry> dataTable;
     private @FXML ListView<TableEntry> tablesList;
+
+    private SqlConnection connection;
+
+    @FXML
+    private void initialize() {
+        MySQL sql = MySQL.getModuleByName("MySQL");
+        connection = sql.createConnection();
+        try {
+            connection.open();
+        } catch (JSchException | SQLException e) {
+            e.printStackTrace();
+            requestStageClose();
+            return;
+        }
+    }
 
     public void setDataTableContent(TableContent content) {
         ObservableList<TableColumn<TableEntry, ?>> columns = dataTable.getColumns();
@@ -45,6 +68,38 @@ public class TabScreen extends VBox {
             }
         });
     }
+
+    @Override
+    public void setStage(Stage stage) {
+        this.stage = stage;
+        stage.setOnCloseRequest(e -> {
+            try {
+                if (connection.isOpen()) connection.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        });
+    }
+
+    private void requestStageClose() {
+        try {
+            if (connection.isOpen())  connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        if (stage != null) stage.close();
+        else new Thread(() -> {
+            while (stage == null)  try {
+                Thread.sleep(20);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            Platform.runLater(stage::close);
+        }).start();
+    }
+
+    private Stage stage = null;
 
     public static TabScreen showTabScreen(Object... lambdaArgs) {
         return View.createNewWindow("/eu/shooktea/vmsm/view/fxml/mysql/TabScreen.fxml", "Database");
