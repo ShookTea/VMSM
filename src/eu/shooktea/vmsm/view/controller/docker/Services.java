43a367f5bd1ab0884412e6d2865e4a57834c8fb4
Norbert Kowalik <nkowalik@divante.pl>
2018-08-13 11:51:27 +0200

    Loading data

--- src/eu/shooktea/vmsm/view/controller/docker/Services.java
+++ src/eu/shooktea/vmsm/view/controller/docker/Services.java
@@ -7,15 +7,21 @@ import eu.shooktea.vmsm.module.dockercompose.ComposeFile;
 import eu.shooktea.vmsm.module.dockercompose.DockerCompose;
 import eu.shooktea.vmsm.module.dockercompose.Service;
 import eu.shooktea.vmsm.view.View;
+import javafx.beans.property.*;
 import javafx.fxml.FXML;
-import javafx.scene.control.ListView;
+import javafx.scene.control.*;
+import org.reactfx.value.Val;
 
 import java.io.File;
 import java.io.IOException;
 
 public class Services {
     @FXML private ListView<Service> servicesListView;
+    @FXML private TextField serviceName;
+    @FXML private TextField serviceSource;
+    @FXML private ChoiceBox<Service.ServiceSource> serviceSourceType;
 
+    private ObjectProperty<Service> currentService = new SimpleObjectProperty<>();
     private DockerCompose dockerCompose;
     private VirtualMachine vm;
     private ComposeFile composeFile;
@@ -30,9 +36,21 @@ public class Services {
             composeFile = new ComposeFile(Yaml.createYamlInput(input).readYamlMapping());
 
             servicesListView.setItems(composeFile.getServices());
+            servicesListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+            currentService.bind(servicesListView.getSelectionModel().selectedItemProperty());
+            serviceSourceType.setItems(Service.ServiceSource.listValues());
+
+            serviceName.textProperty().bindBidirectional(Val.selectVar(currentService, Service::nameProperty));
+            serviceSource.textProperty().bindBidirectional(Val.selectVar(currentService, Service::sourceProperty));
+            serviceSourceType.valueProperty().bindBidirectional(Val.selectVar(currentService, Service::sourceTypeProperty));
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @FXML
+    private void saveServiceData() {
+
     }
 
     public static void openDockerServicesWindow(Object... lambdaArgs) {
