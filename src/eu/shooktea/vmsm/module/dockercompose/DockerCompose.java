7318c270f4ae255c262555578ff5219049d61169
Norbert Kowalik <nkowalik@divante.pl>
2018-08-13 07:48:01 +0200

    Quick GUI button

--- src/eu/shooktea/vmsm/module/dockercompose/DockerCompose.java
+++ src/eu/shooktea/vmsm/module/dockercompose/DockerCompose.java
@@ -1,6 +1,11 @@
 package eu.shooktea.vmsm.module.dockercompose;
 
+import eu.shooktea.vmsm.Toolkit;
 import eu.shooktea.vmsm.module.VMModule;
+import javafx.scene.image.ImageView;
+
+import java.util.Collections;
+import java.util.List;
 
 public class DockerCompose extends VMModule {
     @Override
@@ -11,5 +16,11 @@ public class DockerCompose extends VMModule {
     @Override
     public String getDescription() {
         return "Set of toolkits for Docker Compose";
+    }
+
+    @Override
+    public List<ImageView> getQuickGuiButtons() {
+        ImageView openDocker = Toolkit.createQuickGuiButton("docker_logo.png", "Open Docker Compose");
+        return Collections.singletonList(openDocker);
     }
 }
