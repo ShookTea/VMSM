e1e0eb8705e337762c07e79301504229bc28f677
Norbert Kowalik <nkowalik@divante.pl>
2018-08-13 07:49:28 +0200

    Main menu item

--- src/eu/shooktea/vmsm/module/dockercompose/DockerCompose.java
+++ src/eu/shooktea/vmsm/module/dockercompose/DockerCompose.java
@@ -2,10 +2,12 @@ package eu.shooktea.vmsm.module.dockercompose;
 
 import eu.shooktea.vmsm.Toolkit;
 import eu.shooktea.vmsm.module.VMModule;
+import javafx.scene.control.MenuItem;
 import javafx.scene.image.ImageView;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 public class DockerCompose extends VMModule {
     @Override
@@ -22,5 +24,11 @@ public class DockerCompose extends VMModule {
     public List<ImageView> getQuickGuiButtons() {
         ImageView openDocker = Toolkit.createQuickGuiButton("docker_logo.png", "Open Docker Compose");
         return Collections.singletonList(openDocker);
+    }
+
+    @Override
+    public Optional<MenuItem> getMenuItem() {
+        MenuItem dockerCompose = new MenuItem("Docker Compose", Toolkit.createMenuImage("docker_logo.png"));
+        return Optional.of(dockerCompose);
     }
 }
