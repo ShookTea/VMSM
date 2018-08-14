46c040c2746f41890b6a1ccad38575d8773cdc32
Norbert Kowalik <nkowalik@divante.pl>
2018-08-14 06:43:04 +0200

    Mapping

--- src/eu/shooktea/yaml/YamlValue.java
+++ src/eu/shooktea/yaml/YamlValue.java
@@ -13,12 +13,27 @@ public abstract class YamlValue {
         return type == YamlType.MAP;
     }
 
+    public YamlMap toMap() {
+        if (!isMap() || !(this instanceof YamlMap)) throw new RuntimeException("Value " + this + " is not a map!");
+        return (YamlMap)this;
+    }
+
     public boolean isList() {
         return type == YamlType.LIST;
     }
 
+    public YamlList toList() {
+        if (!isList() || !(this instanceof YamlList)) throw new RuntimeException("Value " + this + " is not a primitive!");
+        return (YamlList)this;
+    }
+
     public boolean isPrimitive() {
         return type == YamlType.PRIMITIVE;
+    }
+
+    public YamlPrimitive<?> toPrimitive() {
+        if (!isList() || !(this instanceof YamlPrimitive)) throw new RuntimeException("Value " + this + " is not a primitive!");
+        return (YamlPrimitive)this;
     }
 
     private final YamlType type;
