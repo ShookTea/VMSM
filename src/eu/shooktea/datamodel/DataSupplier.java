223c4ceabac5b7dc22583df3c960de6ea5e6febb
Norbert Kowalik <nkowalik@divante.pl>
2018-08-14 13:23:38 +0200

    DataSupplier interface

---
+++ src/eu/shooktea/datamodel/DataSupplier.java
@@ -0,0 +1,63 @@
+package eu.shooktea.datamodel;
+
+import java.io.*;
+import java.util.Scanner;
+
+public interface DataSupplier {
+    DataModelMap load(String s);
+    String store(DataModelMap dmm);
+
+    default DataModelMap load(InputStream is) {
+        StringBuilder sb = new StringBuilder();
+        Scanner sc = new Scanner(is);
+        while (sc.hasNextLine()) {
+            sb.append(sc.nextLine()).append(System.lineSeparator());
+        }
+        sc.close();
+        return load(sb.toString());
+    }
+
+    default DataModelMap load(Reader r) {
+        StringBuilder sb = new StringBuilder();
+        Scanner sc = new Scanner(r);
+        while (sc.hasNextLine()) {
+            sb.append(sc.nextLine()).append(System.lineSeparator());
+        }
+        sc.close();
+        return load(sb.toString());
+    }
+
+    default DataModelMap load(File f) {
+        try {
+            return load(new FileInputStream(f));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    default String store(DataModelMap dmm, Writer w) {
+        try {
+            String text = store(dmm);
+            w.append(text);
+            return text;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    default String store(DataModelMap dmm, OutputStream os) {
+        PrintWriter pw = new PrintWriter(os);
+        String text = store(dmm);
+        pw.println(text);
+        pw.close();
+        return text;
+    }
+
+    default String store(DataModelMap dmm, File f) {
+        try {
+            return store(dmm, new FileOutputStream(f));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
