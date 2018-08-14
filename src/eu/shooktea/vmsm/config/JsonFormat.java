b7cfbb622a0473c6c6df9f6764620626ee153355
Norbert Kowalik <nkowalik@divante.pl>
2018-08-14 11:49:01 +0200

    JsonFormat class with loading

---
+++ src/eu/shooktea/vmsm/config/JsonFormat.java
@@ -0,0 +1,76 @@
+package eu.shooktea.vmsm.config;
+
+import eu.shooktea.vmsm.Storage;
+import eu.shooktea.vmsm.VM;
+import eu.shooktea.vmsm.VirtualMachine;
+import javafx.collections.ObservableList;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.util.List;
+
+public class JsonFormat extends AbstractFormat {
+    @Override
+    public void load(File file) throws IOException {
+        if (!file.exists() || !file.isFile()) return;
+        String config = new String(Files.readAllBytes(file.toPath())).trim();
+        if (config.isEmpty()) return;
+
+        JSONObject object = new JSONObject(config);
+        loadVMs(object);
+        loadCurrentVM(object);
+        loadIgnoredVagrantMachines(object);
+        loadVmsmConfig(object);
+    }
+
+    private void loadVMs(JSONObject obj) throws MalformedURLException {
+        Storage.getVmList().clear();
+        if (!obj.has("VMs")) return;
+        for (Object ob : obj.getJSONArray("VMs")) {
+            JSONObject json = (JSONObject)ob;
+            VirtualMachine vm = VirtualMachine.fromJSON(json);
+            Storage.getVmList().add(vm);
+        }
+    }
+
+    private void loadCurrentVM(JSONObject obj) {
+        ObservableList<VirtualMachine> vmList = Storage.getVmList();
+        if (obj.has("current_vm")) {
+            String currentVmName = obj.getString("current_vm");
+            List<VirtualMachine> filtered = vmList.filtered(vm -> vm.getName().equals(currentVmName));
+            if (filtered.size() == 1) {
+                VM.set(filtered.get(0));
+            }
+            else if (vmList.size() > 0) {
+                VM.set(vmList.get(0));
+            }
+        }
+        else if (vmList.size() > 0) {
+            VM.set(vmList.get(0));
+        }
+    }
+
+    private void loadIgnoredVagrantMachines(JSONObject obj) {
+        Storage.getIgnoredVagrantMachines().clear();
+        if (obj.has("ignored_vagrant_machines")) {
+            JSONArray array = obj.getJSONArray("ignored_vagrant_machines");
+            array.iterator().forEachRemaining(entry -> Storage.getIgnoredVagrantMachines().add(entry.toString()));
+        }
+    }
+
+    private void loadVmsmConfig(JSONObject obj) {
+        Storage.config.clear();
+        if (obj.has("config")) {
+            Storage.config.putAll(obj.getJSONObject("config").toMap());
+        }
+    }
+
+    @Override
+    public void save(File file) throws IOException {
+
+    }
+}
