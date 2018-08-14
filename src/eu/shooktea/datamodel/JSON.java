160b929039107b35beb11d6ed84d0db4ae8fdb93
Norbert Kowalik <nkowalik@divante.pl>
2018-08-14 13:50:01 +0200

    Introduction to JSON implementation of DataSupplier

---
+++ src/eu/shooktea/datamodel/JSON.java
@@ -0,0 +1,39 @@
+package eu.shooktea.datamodel;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.function.Function;
+
+public class JSON implements DataSupplier {
+    @Override
+    public DataModelMap load(String s) {
+        return converter().apply(new JSONObject(s)).toMap();
+    }
+
+    @Override
+    public String store(DataModelMap dmm) {
+        return new JSONObject(dmm.toStorageObject()).toString();
+    }
+
+    @Override
+    public Function<Object, DataModelValue> converter() {
+        return ob -> {
+            if (ob instanceof JSONObject) {
+                return new DataModelMap(((JSONObject) ob).toMap());
+            }
+            if (ob instanceof JSONArray) {
+                return new DataModelList(((JSONArray) ob).toList());
+            }
+        };
+    }
+
+    public static JSON instance() {
+        if (instance == null)
+            instance = new JSON();
+        DataModelValue.setConverter(instance.converter());
+        return instance;
+    }
+
+    private static JSON instance = null;
+}
