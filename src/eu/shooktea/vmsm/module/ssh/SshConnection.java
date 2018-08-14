f8724ca0864e12083145986fd72ba039e72d06b9
Norbert Kowalik <nkowalik@divante.pl>
2018-08-14 10:38:12 +0200

    SSH refactorization

--- src/eu/shooktea/vmsm/module/ssh/SshConnection.java
+++ src/eu/shooktea/vmsm/module/ssh/SshConnection.java
@@ -1,14 +1,11 @@
 package eu.shooktea.vmsm.module.ssh;
 
 import com.jcraft.jsch.ChannelShell;
-import com.jcraft.jsch.JSchException;
 import eu.shooktea.fxtoolkit.terminal.TerminalConnection;
 import eu.shooktea.fxtoolkit.terminal.TerminalIO;
 import javafx.application.Platform;
 
 import java.io.OutputStream;
-import java.io.PipedInputStream;
-import java.io.PipedOutputStream;
 import java.io.PrintStream;
 
 /**
@@ -35,59 +32,6 @@ public class SshConnection extends TerminalConnection {
                 print(String.valueOf((char)i));
                 onTerminalUpdate.run();
             });
-        }
-    }
-
-    private static class ChannelShellIO implements TerminalIO {
-        private ChannelShell shell;
-        private OutputStream os;
-        private OutputStream is;
-
-        public ChannelShellIO(ChannelShell shell) {
-            this.shell = shell;
-        }
-
-        @Override
-        public void init() {
-            try {
-                if (shell == null) return;
-                shell.setAgentForwarding(true);
-                shell.setOutputStream(os);
-                shell.setPtySize(200, 500, 800, 600);
-
-                PipedInputStream pin = new PipedInputStream();
-                is = new PipedOutputStream(pin);
-                shell.setInputStream(pin);
-
-                shell.connect(300);
-            } catch (Exception ex) {
-                throw new RuntimeException(ex);
-            }
-        }
-
-        public void setInputStream(OutputStream stream) {
-            this.os = stream;
-        }
-
-        @Override
-        public void close() {
-            try {
-                if (shell.isClosed()) return;
-                shell.disconnect();
-                shell.getSession().disconnect();
-            } catch (JSchException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        @Override
-        public OutputStream getTerminalOutputStream() {
-            return is;
-        }
-
-        @Override
-        public OutputStream getTerminalInputStream() {
-            return os;
         }
     }
 }
