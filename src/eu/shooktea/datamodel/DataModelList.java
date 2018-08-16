176c56faf0248fd8a65b6f0a4ee860aaab711337
Norbert Kowalik <nkowalik@divante.pl>
2018-08-16 07:01:08 +0200

    DataModelList is now a proper list

--- src/eu/shooktea/datamodel/DataModelList.java
+++ src/eu/shooktea/datamodel/DataModelList.java
@@ -1,13 +1,11 @@
 package eu.shooktea.datamodel;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class DataModelList extends DataModelValue implements Iterable<DataModelValue> {
-    public DataModelList(List<Object> objectList) {
+public class DataModelList extends DataModelValue implements List<DataModelValue> {
+    public DataModelList(List<?> objectList) {
         super(DataModelType.LIST);
         this.list = new ArrayList<>();
         for (Object ob : objectList) {
@@ -35,30 +33,76 @@ public class DataModelList extends DataModelValue implements Iterable<DataModelV
                 .collect(Collectors.toList());
     }
 
+    @Override
     public int size() {
         return list.size();
     }
 
+    @Override
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return list.contains(o);
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return list.containsAll(c);
+    }
+
+    @Override
     public DataModelValue get(int i) {
         return list.get(i);
     }
 
-    public void add(DataModelValue val) {
-        list.add(val);
+    @Override
+    public boolean add(DataModelValue val) {
+        return list.add(val);
     }
 
+    @Override
     public void add(int index, DataModelValue val) {
         list.add(index, val);
     }
 
-    public void remove(int index) {
-        list.remove(index);
+    @Override
+    public boolean addAll(Collection<? extends DataModelValue> c) {
+        return list.addAll(c);
     }
 
-    public void remove(DataModelValue val) {
-        list.remove(val);
+    @Override
+    public boolean addAll(int index, Collection<? extends DataModelValue> c) {
+        return list.addAll(index, c);
     }
 
+    @Override
+    public DataModelValue remove(int index) {
+        return list.remove(index);
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return list.remove(o);
+    }
+
+    public boolean remove(DataModelValue val) {
+        return list.remove(val);
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return list.removeAll(c);
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return list.retainAll(c);
+    }
+
+    @Override
     public void clear() {
         list.clear();
     }
@@ -70,6 +114,59 @@ public class DataModelList extends DataModelValue implements Iterable<DataModelV
     @Override
     public Iterator<DataModelValue> iterator() {
         return list.iterator();
+    }
+
+    @Override
+    public DataModelValue[] toArray() {
+        return list.toArray(new DataModelValue[size()]);
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return list.toArray(a);
+    }
+
+    @Override
+    public boolean equals(Object ob) {
+        if (ob == this || ob == list) return true;
+        if (ob == null) return false;
+        if (!(ob instanceof List)) return false;
+        return list.equals(ob);
+    }
+
+    @Override
+    public int hashCode() {
+        return list.hashCode();
+    }
+
+    @Override
+    public DataModelValue set(int index, DataModelValue element) {
+        return list.set(index, element);
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return list.indexOf(o);
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return list.lastIndexOf(o);
+    }
+
+    @Override
+    public ListIterator<DataModelValue> listIterator() {
+        return list.listIterator();
+    }
+
+    @Override
+    public ListIterator<DataModelValue> listIterator(int index) {
+        return list.listIterator(index);
+    }
+
+    @Override
+    public List<DataModelValue> subList(int fromIndex, int toIndex) {
+        return new DataModelList(list.subList(fromIndex, toIndex));
     }
 
     private final List<DataModelValue> list;
